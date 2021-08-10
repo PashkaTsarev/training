91fe8a072bcea274f03d3a5cb683bb619b07e3b0
PashkaTsarev <88406817+PashkaTsarev@users.noreply.github.com>
2021-08-10 07:29:27 +0500

    Click PopUp
    training

---
+++ training/main.js
@@ -0,0 +1,74 @@
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('tel')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            }else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input)
+                error++;
+            }else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+
+        }
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+});
+
